930159d361a9074edc31500b707333e2933f11b3
Moisés J <moisesjbc@gmail.com>
2020-05-16 00:31:46 +0100

    Added very-basic-but-always-correct-IA

--- godot-project/main/player/player.gd
+++ godot-project/main/player/player.gd
@@ -4,12 +4,18 @@ var ColorUtils = preload("res://scripts/color_utils.gd")
 
 var current_color: String = "R"
 export var player_index: int = 1
+export var bot: bool = false
 export var velocity: float = 900
 var direction: Vector2 = Vector2(0.0, 0.0)
 var layer: int = 2
 
+export var ball_path: NodePath
+var ball: Node2D
+
 func _ready():
 	ColorUtils.set_color(self, current_color)
+	ball = get_node(ball_path)
+	print(ball)
 
 
 func _input(_event):
@@ -27,6 +33,15 @@ func _input(_event):
 	elif Input.is_action_just_pressed("ui_blue_player_"+String(player_index)):
 		ColorUtils.set_color(self, "B")
 
-
-func _process(delta):
+func _physics_process(delta):
+	if bot:
+		if ball.position.y < self.position.y:
+			direction.y = -1
+		elif ball.position.y > self.position.y:
+			direction.y = 1
+		else:
+			direction.y = 0
+			
+		ColorUtils.set_color(self, ball.current_color)
+	
 	move_and_collide(velocity * direction * delta)
