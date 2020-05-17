2112809e4385ed91eb400db0c418a33c0efb0998
Moisés J <moisesjbc@gmail.com>
2020-05-17 19:07:03 +0100

    Added prepare countdown

--- godot-project/main/main.gd
+++ godot-project/main/main.gd
@@ -3,7 +3,7 @@ extends Node2D
 func _ready():
 	if globals.n_players == 1:
 		$player_2.start_bot()
-	get_tree().paused = false
+	get_tree().paused = true
 
 
 func _process(_delta):
@@ -12,3 +12,8 @@ func _process(_delta):
 
 func _on_score_player_wins(player):
 	$gui/game_over_menu.run(player)
+
+
+func _on_countdown_timer_timeout():
+	$gui/ready_container.visible = false
+	get_tree().paused = false
