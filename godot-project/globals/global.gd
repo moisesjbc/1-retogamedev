1766515a5f6d05781489d21a271d6c2ff50a5d41
Moisés J <moisesjbc@gmail.com>
2020-05-17 17:30:11 +0100

    Some colors adjustments

--- godot-project/globals/global.gd
+++ godot-project/globals/global.gd
@@ -4,6 +4,8 @@ extends Node
 var n_players = 1
 
 var colors = [
+	[Color("#ff3e3e"), Color("#7bff7b"), Color("#6a6aff")],
+	[Color("#ff6666"), Color("#66ff66"), Color("#6666ff")],
 	[Color("#ff0000"), Color("#00ff00"), Color("#0000ff")],
 	[Color("#786719"), Color("#fff000"), Color("#0059e3")],
 	[Color("#bba300"), Color("#665944"), Color("#2017ff")],
